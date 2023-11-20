73a52c98cbf818916c3eeca342c70264f7dc3fd5
BERKABZ <150333064+BERKABZ@users.noreply.github.com>
2023-11-20 09:53:18 +0800

    Add files via upload

---
+++ berber/src/main/java/com/mycompany/berber/Berber.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.berber;
+
+/**
+ *
+ * @author user
+ */
+public class Berber {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
